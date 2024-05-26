9264968ef44d70945206a09409a74748c43a0626
agnuscrucifixion <nikitabysko123@gmail.com>
2024-05-26 20:43:35 +0300

    first commit

---
+++ src/test/java/com/example/gameagregator/GameAgregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameagregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAgregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
