9ba67100f0c537a25a5c417e1685ba18521f87e6
agnuscrucifixion <nikitabysko123@gmail.com>
2024-05-31 22:10:56 +0300

    little corrections in react

--- front/src/App.js
+++ front/src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UserList from './components/UserList';
 import GameList from './components/GameList';
 import CollectionList from './components/CollectionList';
+import FileHandler from "./components/FileHandler";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/users" component={UserList} />
-            <Route path="/games" component={GameList} />
-            <Route path="/collections" component={CollectionList} />
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/users" component={UserList}/>
+                    <Route path="/games" component={GameList}/>
+                    <Route path="/collections" component={CollectionList}/>
+                    <FileHandler/>
+
+                </Switch>
+            </div>
+        </Router>
+    );
+
 }
 
 export default App;
